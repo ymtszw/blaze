7ae8d09c3529725e3bb0e9af390a8d801ab68613
ymtszw <ymtszw@gmail.com>
2017-10-27 20:26:15 +0900

    [master] Remove account-specific info from example xml

--- tests/XmlTest.elm
+++ tests/XmlTest.elm
@@ -77,9 +77,7 @@ exampleXmlElement =
             , Element "RequestId" [] [ Text "93498126-da8f-4d62-945f-d6719e8c2a25" ]
             , Element "Arguments"
                 []
-                [ Element "Argument" [ { name = "Name", value = "AWSAccessKeyId" }, { name = "Value", value = "AKIAJV3IQTOHAKNURUIA" } ] []
-                , Element "Argument" [ { name = "Name", value = "AssociateTag" }, { name = "Value", value = "blaze06-22" } ] []
-                , Element "Argument" [ { name = "Name", value = "BrowseNode" }, { name = "Value", value = "2250739051" } ] []
+                [ Element "Argument" [ { name = "Name", value = "BrowseNode" }, { name = "Value", value = "2250739051" } ] []
                 , Element "Argument" [ { name = "Name", value = "Keywords" }, { name = "Value", value = "衿沢世衣子" } ] []
                 , Element "Argument" [ { name = "Name", value = "Operation" }, { name = "Value", value = "ItemSearch" } ] []
                 , Element "Argument" [ { name = "Name", value = "SearchIndex" }, { name = "Value", value = "KindleStore" } ] []
@@ -107,46 +105,9 @@ exampleXmlElement =
                 ]
             , Element "TotalResults" [] [ Text "48" ]
             , Element "TotalPages" [] [ Text "5" ]
-            , Element "MoreSearchResultsUrl"
-                []
-                [ Text "https://www.amazon.co.jp/gp/search?linkCode=xm2&node=2250739051&SubscriptionId=AKIAJV3IQTOHAKNURUIA&keywords=%E8%A1%BF%E6%B2%A2%E4%B8%96%E8%A1%A3%E5%AD%90&tag=blaze06-22&creative=5143&url=search-alias%3Ddigital-text&camp=2025" ]
             , Element "Item"
                 []
                 [ Element "ASIN" [] [ Text "B073XHZBSH" ]
-                , Element "DetailPageURL"
-                    []
-                    [ Text "https://www.amazon.co.jp/%E3%83%A2%E3%83%BC%E3%83%8B%E3%83%B3%E3%82%B0-2017%E5%B9%B434%E5%8F%B7-2017%E5%B9%B47%E6%9C%8820%E6%97%A5%E7%99%BA%E5%A3%B2-%E9%9B%91%E8%AA%8C-%E6%B1%9F%E5%8F%A3%E5%A4%8F%E5%AE%9F-ebook/dp/B073XHZBSH?SubscriptionId=AKIAJV3IQTOHAKNURUIA&tag=blaze06-22&linkCode=xm2&camp=2025&creative=165953&creativeASIN=B073XHZBSH" ]
-                , Element "ItemLinks"
-                    []
-                    [ Element "ItemLink"
-                        []
-                        [ Element "Description" [] [ Text "Add To Wishlist" ]
-                        , Element "URL"
-                            []
-                            [ Text "https://www.amazon.co.jp/gp/registry/wishlist/add-item.html?asin.0=B073XHZBSH&SubscriptionId=AKIAJV3IQTOHAKNURUIA&tag=blaze06-22&linkCode=xm2&camp=2025&creative=5143&creativeASIN=B073XHZBSH" ]
-                        ]
-                    , Element "ItemLink"
-                        []
-                        [ Element "Description" [] [ Text "Tell A Friend" ]
-                        , Element "URL"
-                            []
-                            [ Text "https://www.amazon.co.jp/gp/pdp/taf/B073XHZBSH?SubscriptionId=AKIAJV3IQTOHAKNURUIA&tag=blaze06-22&linkCode=xm2&camp=2025&creative=5143&creativeASIN=B073XHZBSH" ]
-                        ]
-                    , Element "ItemLink"
-                        []
-                        [ Element "Description" [] [ Text "All Customer Reviews" ]
-                        , Element "URL"
-                            []
-                            [ Text "https://www.amazon.co.jp/review/product/B073XHZBSH?SubscriptionId=AKIAJV3IQTOHAKNURUIA&tag=blaze06-22&linkCode=xm2&camp=2025&creative=5143&creativeASIN=B073XHZBSH" ]
-                        ]
-                    , Element "ItemLink"
-                        []
-                        [ Element "Description" [] [ Text "All Offers" ]
-                        , Element "URL"
-                            []
-                            [ Text "https://www.amazon.co.jp/gp/offer-listing/B073XHZBSH?SubscriptionId=AKIAJV3IQTOHAKNURUIA&tag=blaze06-22&linkCode=xm2&camp=2025&creative=5143&creativeASIN=B073XHZBSH" ]
-                        ]
-                    ]
                 , Element "ItemAttributes"
                     []
                     [ Element "Author" [] [ Text "江口夏実" ]
@@ -186,40 +147,6 @@ exampleXmlElement =
             , Element "Item"
                 []
                 [ Element "ASIN" [] [ Text "B073PWP7Y7" ]
-                , Element "DetailPageURL"
-                    []
-                    [ Text "https://www.amazon.co.jp/%E3%80%8E%E3%83%87%E3%83%93%E3%83%AB%E3%82%BA%E3%83%A9%E3%82%A4%E3%83%B3%E3%80%8F%E3%82%A2%E3%83%8B%E3%83%A1%E5%8C%96%E8%A8%98%E5%BF%B5-%E6%81%8B%E3%81%99%E3%82%8B%E4%BA%BA%E5%A4%96-%E8%A9%A6%E3%81%97%E8%AA%AD%E3%81%BF%E7%84%A1%E6%96%99%E3%83%91%E3%83%83%E3%82%AF-%E8%8A%B1%E7%94%B0%E9%99%B5-ebook/dp/B073PWP7Y7?SubscriptionId=AKIAJV3IQTOHAKNURUIA&tag=blaze06-22&linkCode=xm2&camp=2025&creative=165953&creativeASIN=B073PWP7Y7" ]
-                , Element "ItemLinks"
-                    []
-                    [ Element "ItemLink"
-                        []
-                        [ Element "Description" [] [ Text "Add To Wishlist" ]
-                        , Element "URL"
-                            []
-                            [ Text "https://www.amazon.co.jp/gp/registry/wishlist/add-item.html?asin.0=B073PWP7Y7&SubscriptionId=AKIAJV3IQTOHAKNURUIA&tag=blaze06-22&linkCode=xm2&camp=2025&creative=5143&creativeASIN=B073PWP7Y7" ]
-                        ]
-                    , Element "ItemLink"
-                        []
-                        [ Element "Description" [] [ Text "Tell A Friend" ]
-                        , Element "URL"
-                            []
-                            [ Text "https://www.amazon.co.jp/gp/pdp/taf/B073PWP7Y7?SubscriptionId=AKIAJV3IQTOHAKNURUIA&tag=blaze06-22&linkCode=xm2&camp=2025&creative=5143&creativeASIN=B073PWP7Y7" ]
-                        ]
-                    , Element "ItemLink"
-                        []
-                        [ Element "Description" [] [ Text "All Customer Reviews" ]
-                        , Element "URL"
-                            []
-                            [ Text "https://www.amazon.co.jp/review/product/B073PWP7Y7?SubscriptionId=AKIAJV3IQTOHAKNURUIA&tag=blaze06-22&linkCode=xm2&camp=2025&creative=5143&creativeASIN=B073PWP7Y7" ]
-                        ]
-                    , Element "ItemLink"
-                        []
-                        [ Element "Description" [] [ Text "All Offers" ]
-                        , Element "URL"
-                            []
-                            [ Text "https://www.amazon.co.jp/gp/offer-listing/B073PWP7Y7?SubscriptionId=AKIAJV3IQTOHAKNURUIA&tag=blaze06-22&linkCode=xm2&camp=2025&creative=5143&creativeASIN=B073PWP7Y7" ]
-                        ]
-                    ]
                 , Element "ItemAttributes"
                     []
                     [ Element "Author" [] [ Text "花田陵" ]
@@ -246,40 +173,6 @@ exampleXmlElement =
             , Element "Item"
                 []
                 [ Element "ASIN" [] [ Text "B0716SQ575" ]
-                , Element "DetailPageURL"
-                    []
-                    [ Text "https://www.amazon.co.jp/%E3%81%86%E3%81%A1%E3%81%AE%E3%82%AF%E3%83%A9%E3%82%B9%E3%81%AE%E5%A5%B3%E5%AD%90%E3%81%8C%E3%83%A4%E3%83%90%E3%81%84-%E5%88%86%E5%86%8A%E7%89%88%EF%BC%88%EF%BC%91%EF%BC%92%EF%BC%89-%E3%80%8C%E3%83%AA%E3%83%A5%E3%82%A6%E3%81%A8%E3%83%A9%E3%83%B3%E3%82%BF%E3%83%B3%E3%81%AE%E7%81%AF%E3%82%8A%E3%80%8D-%E5%B0%91%E5%B9%B4%E3%83%9E%E3%82%AC%E3%82%B8%E3%83%B3%E3%82%A8%E3%83%83%E3%82%B8%E3%82%B3%E3%83%9F%E3%83%83%E3%82%AF%E3%82%B9-%E8%A1%BF%E6%B2%A2%E4%B8%96%E8%A1%A3%E5%AD%90-ebook/dp/B0716SQ575?SubscriptionId=AKIAJV3IQTOHAKNURUIA&tag=blaze06-22&linkCode=xm2&camp=2025&creative=165953&creativeASIN=B0716SQ575" ]
-                , Element "ItemLinks"
-                    []
-                    [ Element "ItemLink"
-                        []
-                        [ Element "Description" [] [ Text "Add To Wishlist" ]
-                        , Element "URL"
-                            []
-                            [ Text "https://www.amazon.co.jp/gp/registry/wishlist/add-item.html?asin.0=B0716SQ575&SubscriptionId=AKIAJV3IQTOHAKNURUIA&tag=blaze06-22&linkCode=xm2&camp=2025&creative=5143&creativeASIN=B0716SQ575" ]
-                        ]
-                    , Element "ItemLink"
-                        []
-                        [ Element "Description" [] [ Text "Tell A Friend" ]
-                        , Element "URL"
-                            []
-                            [ Text "https://www.amazon.co.jp/gp/pdp/taf/B0716SQ575?SubscriptionId=AKIAJV3IQTOHAKNURUIA&tag=blaze06-22&linkCode=xm2&camp=2025&creative=5143&creativeASIN=B0716SQ575" ]
-                        ]
-                    , Element "ItemLink"
-                        []
-                        [ Element "Description" [] [ Text "All Customer Reviews" ]
-                        , Element "URL"
-                            []
-                            [ Text "https://www.amazon.co.jp/review/product/B0716SQ575?SubscriptionId=AKIAJV3IQTOHAKNURUIA&tag=blaze06-22&linkCode=xm2&camp=2025&creative=5143&creativeASIN=B0716SQ575" ]
-                        ]
-                    , Element "ItemLink"
-                        []
-                        [ Element "Description" [] [ Text "All Offers" ]
-                        , Element "URL"
-                            []
-                            [ Text "https://www.amazon.co.jp/gp/offer-listing/B0716SQ575?SubscriptionId=AKIAJV3IQTOHAKNURUIA&tag=blaze06-22&linkCode=xm2&camp=2025&creative=5143&creativeASIN=B0716SQ575" ]
-                        ]
-                    ]
                 , Element "ItemAttributes"
                     []
                     [ Element "Author" [] [ Text "衿沢世衣子" ]
