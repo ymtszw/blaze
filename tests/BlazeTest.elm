b90fa9e483a9821be82570ca83dbb59e16163553
ymtszw <ymtszw@gmail.com>
2017-12-31 17:01:54 +0900

    [master] Add test stub

---
+++ tests/BlazeTest.elm
@@ -0,0 +1,12 @@
+module BlazeTest exposing (..)
+
+import Expect exposing (..)
+import Fuzz exposing (..)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    test "Blaze" <|
+        \_ ->
+            pass
