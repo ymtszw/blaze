26dfcf6e49ae120248f9996591b9ec6bc9341244
ymtszw <ymtszw@gmail.com>
2018-01-11 23:07:19 +0900

    [master] Simplify webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,34 @@
 const path = require('path')
 
 const BASE = path.join(__dirname, 'dist')
-const EXCLUDED = [
-  /elm-stuff/,
-  /node_modules/,
-]
+const EXCLUDED = [/elm-stuff/, /node_modules/]
 
-module.exports = () => {
-  const node_env = (process.env.NODE_ENV == 'prod') ? 'prod' : 'dev'
+module.exports = {
+  entry: {
+    igniter: ['./src/igniter.js'], // Crawler
+    worker: ['./src/worker.js'], // Worker for Qiita article
+  },
 
-  return {
-    entry: {
-      igniter: ['./src/igniter.js'], // Crawler
-      worker: ['./src/worker.js'], // Worker for Qiita article
-    },
+  target: 'node',
 
-    target: 'node',
+  output: {
+    path: BASE,
+    filename: '[name].js',
+  },
 
-    output: {
-      path: BASE,
-      filename: '[name].js',
-    },
-
-    module: {
-      rules: [
-        {
-          test: /\.elm$/,
-          exclude: EXCLUDED,
-          use: {
-            loader: 'elm-webpack-loader',
-            options: {
-              pathToMake: './bin/unbuffered-elm-make', // Trick elm-make to emit ANSI-colored output
-              debug: node_env == 'dev',
-              warn: true,
-            },
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: EXCLUDED,
+        use: {
+          loader: 'elm-webpack-loader',
+          options: {
+            pathToMake: './bin/unbuffered-elm-make', // Trick elm-make to emit ANSI-colored output
+            warn: true,
           },
         },
-      ],
-    },
-  }
+      },
+    ],
+  },
 }
